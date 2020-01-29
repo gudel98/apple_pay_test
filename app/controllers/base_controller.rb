ce4b2e0ae01be1691f8ecaf7568d9c801e0eda9a
Egor Shpalov <gudel1998@rambler.ru>
2020-01-29 23:34:02 +0000

    Refactor Ajax invocation, add capture, void

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,11 +1,36 @@
 class BaseController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: %i[refund]
+  skip_before_action :verify_authenticity_token, only: %i[capture void refund]
 
   def index
   end
 
+  def capture
+    sleep 0.5
+    status = 200
+    currency = 'USD'
+    message = "#{params[:parent_uid]}: #{params[:amount]} #{currency} was successfully captured"
+    render json: { status: status, message: message }
+  end
+
+  def void
+    sleep 0.5
+    status = 200
+    currency = 'USD'
+    message = "#{params[:parent_uid]}: #{params[:amount]} #{currency} was successfully voided"
+    render json: { status: status, message: message }
+  end
+
   def refund
-    sleep 4
-    {status: 200, message: 'Successfully refunded'}
+    sleep 0.5
+    status = 200
+    currency = 'USD'
+    message = "#{params[:parent_uid]}: #{params[:amount]} #{currency} was successfully refunded"
+    render json: { status: status, message: message }
+  end
+
+  private
+
+  def money_format(amount)
+    # Money.new()
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get  'base/index'
   root 'base#index'
 
-  post '/refund', to: 'base#refund'
+  post '/capture', to: 'base#capture'
+  post '/void',    to: 'base#void'
+  post '/refund',  to: 'base#refund'
 end
