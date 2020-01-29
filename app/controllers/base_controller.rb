eb67cd622b1a28eccbc3bca295029ff9ca65d920
Egor Shpalov <gudel1998@rambler.ru>
2020-01-29 19:25:09 +0000

    Add Ajax request to refund (with alerts)

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,4 +1,11 @@
 class BaseController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: %i[refund]
+
   def index
+  end
+
+  def refund
+    sleep 4
+    {status: 200, message: 'Successfully refunded'}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'base/index'
+  get  'base/index'
   root 'base#index'
+
+  post '/refund', to: 'base#refund'
 end
