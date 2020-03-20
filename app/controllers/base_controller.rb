58edf7bfa864ebfa3cae5f756339deba68d1e1d5
egor <egorshpalov1998@gmail.com>
2020-03-20 14:26:36 +0300

    Update redirect url

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -100,7 +100,7 @@ class BaseController < ApplicationController
 
     response = ctp_connection.post('https://checkout-staging.begateway.com/ctp/api/checkouts', data.to_json)
     response = JSON.parse(response.body)['checkout']
-    redirect_to "https://#{URI(response['redirect_url']).host}/widget/widget_launch.html?token=#{response['token']}"
+    redirect_to "https://#{URI(response['redirect_url']).host}/widget/hpp.html?token=#{response['token']}"
   rescue Faraday::ConnectionFailed => error
     render json: { status: 500, message: error.message }
   rescue JSON::ParserError, TypeError
