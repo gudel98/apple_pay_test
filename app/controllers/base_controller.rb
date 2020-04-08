cba02c77e31fc16da41a79b1e7ff0ead81364989
egor <egorshpalov1998@gmail.com>
2020-04-08 18:09:09 +0300

    Add amount for voids

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -29,6 +29,7 @@ class BaseController < ApplicationController
 
   def void
     data = {
+      amount: money_format(params[:amount]),
       parent_uid: params[:parent_uid]
     }
     commit('void', data)
